2336c610ea93b104e9718b8e875c0b68bdbf605d
huangwwmm <hwm@huangwm.com>
2018-11-08 16:07:22 +0800

    add Debug.Assert and logRecord

---
+++ Assets/Scripts/snLogRecord.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public class snLogRecord
+{
+	private FileStream m_FileStream;
+	private StreamWriter m_StreamWriter;
+
+	/// <summary>
+	/// avoid multiple registered <see cref="OnLog"/>
+	/// </summary>
+	private bool m_IsRegisteredOnLogEvent = false;
+
+	private byte m_RecordLogTypeFlags = byte.MaxValue;
+	private bool m_RecordStackTrace = true;
+
+	public void Initialize(byte recordLogTypeFlag, bool recordStackTrace)
+	{
+		m_RecordLogTypeFlags = recordLogTypeFlag;
+		m_RecordStackTrace = recordStackTrace;
+
+		string logDirectory;
+
+		string logFile;
+#if UNITY_EDITOR || UNITY_STANDALONE
+		logDirectory = Application.dataPath + "/../Temp/LogRecord/";
+		logFile = DateTime.Now.ToString("yyyy-M-dd--HH-mm-ss") + ".log";
+#endif
+
+		string logPath = logDirectory + logFile;
+		Debug.Log("LogRecord path: " + logPath);
+		try
+		{
+			if (!Directory.Exists(logDirectory))
+			{
+				Directory.CreateDirectory(logDirectory);
+			}
+
+			m_FileStream = new FileStream(logPath, FileMode.CreateNew, FileAccess.Write, FileShare.Read);
+			m_StreamWriter = new StreamWriter(m_FileStream);
+			m_StreamWriter.AutoFlush = false; // for better performance
+			StartReceivedLog();
+		}
+		catch (Exception e)
+		{
+			snDebug.Assert(false, "Create LogRecord file failed. Exception:\n" + e.ToString());
+		}
+	}
+
+	public void Destroy()
+	{
+		StopReceivedLog();
+
+		FlushStreamWriter();
+		m_StreamWriter.Close();
+		m_FileStream.Flush();
+		m_FileStream.Close();
+	}
+
+	/// <summary>
+	/// call this function instead of auto flush for better preformance
+	/// </summary>
+	public void FlushStreamWriter()
+	{
+		m_StreamWriter.Flush();
+	}
+
+	public void StartReceivedLog()
+	{
+		if (!m_IsRegisteredOnLogEvent)
+		{
+			Application.logMessageReceived += OnLog;
+			m_IsRegisteredOnLogEvent = true;
+		}
+	}
+
+	public void StopReceivedLog()
+	{
+		if (m_IsRegisteredOnLogEvent)
+		{
+			Application.logMessageReceived -= OnLog;
+			m_IsRegisteredOnLogEvent = false;
+		}
+	}
+
+	private void OnLog(string condition, string stackTrace, LogType type)
+	{
+		if ((m_RecordLogTypeFlags & (1 << (byte)type)) > 0)
+		{
+			m_StreamWriter.WriteLine(string.Format("{0}|{1}|{2}", DateTime.Now.ToString("G"), type, condition));
+			if (m_RecordStackTrace)
+			{
+				m_StreamWriter.WriteLine(stackTrace);
+			}
+		}
+	}
+}
--- Assets/Scripts/snSystem.cs
+++ Assets/Scripts/snSystem.cs
@@ -1,14 +1,24 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class snSystem : MonoBehaviour
 {
 	private static snSystem ms_Instance;
 
+	public Object[] SystemPrefabs;
+
+	private snLogRecord m_LogRecord;
 	private float m_RealtimeSinceStartup;
+
 
 	public static snSystem GetInstance()
 	{
 		return ms_Instance;
+	}
+
+	public snLogRecord GetLogRecord()
+	{
+		return m_LogRecord;
 	}
 
 	public float GetRealtimeSinceStartup()
@@ -18,16 +28,60 @@ public class snSystem : MonoBehaviour
 
 	protected void Awake()
 	{
-		ms_Instance = this;
+		if (ms_Instance == null) // avoid multiple snSystem
+		{
+			ms_Instance = this;
+			DontDestroyOnLoad(this);
+
+			StartCoroutine(Initialize_Co());
+		}
+		else
+		{
+			DestroyImmediate(this);
+		}
 	}
 
 	protected void OnDestroy()
 	{
-		ms_Instance = null;
+		if (ms_Instance == this) // avoid multiple snSystem
+		{
+			m_LogRecord.Destroy();
+			m_LogRecord = null;
+
+			ms_Instance = null;
+		}
 	}
 
 	protected void Update()
 	{
 		m_RealtimeSinceStartup = Time.realtimeSinceStartup;
+
+		m_LogRecord.FlushStreamWriter();
+	}
+
+	private IEnumerator Initialize_Co()
+	{
+		m_LogRecord = new snLogRecord();
+#if UNITY_EDITOR
+		m_LogRecord.Initialize(byte.MaxValue, true);
+#else
+		m_LogRecord.Initialize((1 << (byte)LogType.Assert)
+				| (1 << (byte)LogType.Assert)
+				| (1 << (byte)LogType.Exception)
+				| (1 << (byte)LogType.Error)
+				| (1 << (byte)LogType.Warning)
+				| (1 << (byte)LogType.Log)
+			, false);
+#endif
+		yield return null;
+
+		// initizlize system prefab
+		for (int iPrefab = 0; iPrefab < SystemPrefabs.Length; iPrefab++)
+		{
+			Object iterPrefab = SystemPrefabs[iPrefab];
+			GameObject obj = Instantiate(iterPrefab) as GameObject;
+			obj.transform.SetParent(transform, false);
+			yield return null;
+		}
 	}
 }
